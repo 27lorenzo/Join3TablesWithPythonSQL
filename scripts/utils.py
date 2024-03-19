73039e0ecbb1bd5181d8c0166033979d21809935
Lorenzo Borreguero <27lorenzobc@gmail..com>
2024-03-19 11:35:28 +0100

    Changed output file path

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,7 +5,7 @@ def write_to_csv(df, filename):
     """
     Save DataFrame to a CSV file in the 'output' folder
     """
-    output_folder = 'output'
+    output_folder = '../output'
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     filepath = os.path.join(output_folder, filename)
